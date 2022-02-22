ea0410bf5c7dfab50db35caad07b776a6bdedbe9
Ben Martin <1350682+bm9k@users.noreply.github.com>
2022-02-23 05:53:32 +1000

    Add crude implementation of link organiser

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import { useState } from "react";
+
+function useLocalStorage(key, initialValue) {
+  const [value, setStateValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
+
+  const setValue = (value) => {
+    setStateValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+
+  return [value, setValue];
+}
+
+function fisherYates(array) {
+  var count = array.length,
+    randomnumber,
+    temp;
+  while (count) {
+    randomnumber = (Math.random() * count--) | 0;
+    temp = array[count];
+    array[count] = array[randomnumber];
+    array[randomnumber] = temp;
+  }
+}
+
+export default function App() {
+  const [lines, setLines] = useLocalStorage("lines", "");
+  const [output, setOutput] = useState(0);
+
+  const links = lines.split("\n").filter((line) => line.trim());
+  fisherYates(links);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setOutput(Math.random())}>Shuffle</button>
+      <ol>
+        {links.map((link) => {
+          return (
+            <li key={link}>
+              <a href={link}>{link}</a>
+            </li>
+          );
+        })}
+      </ol>
+      <textarea
+        rows="10"
+        style={{ width: "100%" }}
+        onChange={(e) => setLines(e.target.value)}
+        value={lines}
+      ></textarea>
     </div>
   );
 }
-
-export default App;
