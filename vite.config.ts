27580999d5604fd0bb41873d6475aad82c3fc396
Ben Martin <1350682+bm9k@users.noreply.github.com>
2022-07-28 07:58:20 +1000

    Fix base path for github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/link-organiser/",
   plugins: [react({
     jsxRuntime: "classic"
-  })]
+  })],
 })
